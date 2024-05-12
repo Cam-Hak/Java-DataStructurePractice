626eda5c63dde540d6c2c92db7c70efca126a603
Cameron Hakenson <camhakenson@gmail.com>
2024-05-12 11:42:45 -0400

    finished stacks

--- stacks/stacks.java
+++ stacks/stacks.java
@@ -3,15 +3,33 @@ import java.util.Stack;
 public class stacks {
 
   public static void main(String[] args) {
+
+    /* LIFO (Last in first out) */
+
     Stack<String> stack = new Stack<String>();
-    stack.add("cameron");
-    stack.add("hakenson");
 
-    System.out.println(stack.toString());
+    stack.push("Orioles");
+    stack.push("Nationals");
+    stack.push("Yankees");
+    stack.push("Blue Jays");
+    stack.push("Diamondbacks");
 
-    stack.pop();
+    System.out.println(stack.isEmpty()); // should return false
 
-    System.out.println(stack.toString());
+    stack.pop(); // removes top/most recent element from array
+
+    System.out.println(stack);
+
+    /******************************************************************/
+
+    // you can stack a popped element into a var
+    String teamName = stack.pop();
+
+    // prints top element in stack
+    System.out.println(stack.peek());
+
+    // prints out the integer for stack position (-1 if not found in stack)
+    System.out.println(stack.search("Orioles"));
   }
 
 }

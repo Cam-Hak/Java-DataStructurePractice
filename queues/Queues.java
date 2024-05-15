0233c5fa1002d8a26fba2d5706b093107c855f6c
Cameron Hakenson <camhakenson@gmail.com>
2024-05-15 09:20:07 -0400

    finished basic queues

--- queues/Queues.java
+++ queues/Queues.java
@@ -5,13 +5,24 @@ public class Queues {
 
   public static void main(String[] args) {
 
+    /* FIFO data structure */
+
     Queue<String> queue = new LinkedList<String>();
 
+    /* .offer adds to the queue */
     queue.offer("Joe");
     queue.offer("Cameron");
     queue.offer("Steve");
 
-    System.out.println(queue);
+    /* shows whatever is the first element in the queue */
+    System.out.println(queue.peek());
 
+    /* .poll takes away whatever is at the front of the queue */
+    queue.poll();
+
+    /************* Useful Methods *************/
+    System.out.println(queue.isEmpty());
+    System.out.println(queue.size());
+    System.out.println(queue.contains("Cameron"));
   }
 }

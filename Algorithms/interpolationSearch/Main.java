b503a4d922eb254f846884ea7888f36f64a49564
Cameron Hakenson <camhakenson@gmail.com>
2024-05-17 18:33:53 -0400

    interpolation search

---
+++ Algorithms/interpolationSearch/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+  public static void main(String[] args) {
+    /*************** Interpolation Search ***************/
+
+    /*
+     * average case runtime: O(log(log(n)))
+     * worst case runtime: O(n) [values increase exponentially]
+     *
+     * Advantages:
+     * Efficient for large arrays
+     * Very quick for uniformly distributed arrays
+     *
+     * Disadvantages:
+     * Requires a uniform dataset
+     */
+
+    int[] array = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 };
+
+    int index = interpolationSearch(array, 256);
+
+    if (index != -1) {
+      System.out.println("Element found at index " + index);
+    } else {
+      System.out.println("Element not found");
+    }
+  }
+
+  private static int interpolationSearch(int[] array, int value) {
+
+    int high = array.length - 1;
+    int low = 0;
+
+    while (value >= array[low] && value <= array[high] && low <= high) {
+
+      int probe = low + (high - low) * (value - array[low]) /
+          (array[high] - array[low]);
+
+      System.out.println("probe: " + probe);
+
+      if (array[probe] == value) {
+        return probe;
+      } else if (array[probe] < value) {
+        low = probe + 1;
+      } else {
+        high = probe - 1;
+      }
+    }
+
+    return -1;
+  }
+}
